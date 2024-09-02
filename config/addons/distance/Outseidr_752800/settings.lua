a1ed0b385ecc0832a65e7829837b924c65c9911a
Outseidr <tgilbertson88@gmail.com>
2024-09-01 19:15:30 -0500

    Testing
    Testing

---
+++ config/addons/distance/Outseidr_752800/settings.lua
@@ -0,0 +1,13 @@
+require('common');
+
+local settings = T{ };
+settings["font"] = T{ };
+settings["font"]["font_height"] = 32;
+settings["font"]["font_family"] = "Arial";
+settings["font"]["bold"] = true;
+settings["font"]["color"] = 4294967295;
+settings["font"]["visible"] = true;
+settings["font"]["position_y"] = 40;
+settings["font"]["position_x"] = -360;
+
+return settings;
